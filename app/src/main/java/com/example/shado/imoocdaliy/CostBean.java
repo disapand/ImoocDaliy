4c581203c63fa8b3dbe84096695934abf2afb934
shado <shadow724156286@hotmail.com>
2017-03-21 21:51:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shado/imoocdaliy/CostBean.java
@@ -0,0 +1,13 @@
+package com.example.shado.imoocdaliy;
+
+/**
+ * Created by shado on 2017/3/13.
+ */
+
+public class CostBean {
+
+    public  String costTitle;
+    public  String costDate;
+    public  String costMoney;
+
+}
